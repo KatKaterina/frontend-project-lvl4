61922132d7924cb88e88e8f67e4ee076f35e77fb
Salnikova Katerina <katya-salnikova@mail.ru>
2022-03-16 16:49:13 +0300

    ref

--- src/init.jsx
+++ src/init.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import App from './components/App.jsx';
-//import store from './slices/index.jsx';
+import store from './slices/index.js';
 
 export default () => {
 console.log('!');
