87dad748b74bc4fd454fc54b447878b81e5fbfc5
Salnikova Katerina <katya-salnikova@mail.ru>
2022-03-16 17:53:24 +0300

    ref

--- src/validateSchema.js
+++ src/validateSchema.js
@@ -1,8 +1,7 @@
 import * as Yup from 'yup';
 
 const schema = Yup.object().shape({
-  userName: Yup.string()
-    .required(),
+  userName: Yup.string(),
   password: Yup.string()
     .min(6, 'Too Short! Minimum 6 symbols')
     .required(),
