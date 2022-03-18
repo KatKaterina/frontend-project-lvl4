968e66cf5e9fc31f4e82df3d7d0f70808c6350b8
Salnikova Katerina <katya-salnikova@mail.ru>
2022-03-18 20:31:57 +0300

    ref components

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import authorizContext  from '../contexts/index.js';
 
-const Chat = () => (
+const Chat = () => {
+  const authorization = useContext(authorizContext);
+  return (
   <div className="text-center">
     <h1>здесь чат!</h1>
+    <span>
+      <Link onClick={authorization.logOut}>LogOut</Link>
+    </span>
   </div>
 );
+}
 
 export default Chat;
