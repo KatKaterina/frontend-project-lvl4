2a8e8d03a9e9785c45bb51aa3764c1ad56395ae4
Salnikova Katerina <katya-salnikova@mail.ru>
2022-03-19 16:51:03 +0300

    ref components and slices

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -2,6 +2,7 @@ import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { Row } from 'react-bootstrap';
 import authorizContext  from '../contexts/index.js';
+import Channels from './Channels.js';
 
 const Chat = () => {
   const authorization = useContext(authorizContext);
