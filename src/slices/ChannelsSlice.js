5f21f4d584d248b1cac9ebddd1ae93ad5f7f51e9
Salnikova Katerina <katya-salnikova@mail.ru>
2022-03-19 23:29:12 +0300

    ref components

--- src/slices/ChannelsSlice.js
+++ src/slices/ChannelsSlice.js
@@ -54,6 +54,6 @@ const channelsSlice = createSlice ({
 });
 
 export const {actions} = channelsSlice;
-export const selectAllChannels = channelsAdapter.getSelectors((state) => state.channels);
+export const selectAllChannels = channelsAdapter.getSelectors((state) => state);
 //export const { selectAll: selectAllChannels } = channelsAdapter.getSelectors((state) => state.channels);
 export default channelsSlice.reducer;
