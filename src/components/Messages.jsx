e9e938851217d4bec354383a7c8b4f5fe3ec1e79
Salnikova Katerina <katya-salnikova@mail.ru>
2022-03-20 17:34:43 +0300

    ref components

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -1,6 +1,12 @@
 import React, { useEffect } from 'react';
 import { Col, Nav, Button } from 'react-bootstrap';
 import { useSelector, useDispatch } from 'react-redux';
+import { selectorChannels } from '../slices/ChannelsSlice.js';
+
+const channels = useSelector(selectorChannels.selectAll);
+const {currentChannelId} = useSelector((state) => state.channels);
+
+const currentChannelName = channels.filter(({id}) => id === currentChannelId).map((channel) => channel.name);
 
 const Message = () => {
     return (
@@ -10,10 +16,19 @@ const Message = () => {
     )
 }
 
+const MessageHeader = () => {
+  return (
+    <div className="bg-light mb-4 p-3 shadow-sm small">
+      <p className="m-0">{currentChannelName}</p>
+    </div>
+  )
+}
+
 const Messages = () => {
   return (
-  <Col className="h-100">
+  <Col className="h-100 p-0">
       <div className="d-flex flex-column h-100">
+        <MessageHeader />
         <h1>messages</h1>
       </div>
   </Col>
