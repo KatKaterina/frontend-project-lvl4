d88903491a04d3c9e80b7cf78b87caac264a28ce
Salnikova Katerina <katya-salnikova@mail.ru>
2022-03-20 17:51:36 +0300

    ref components

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -7,28 +7,37 @@ const Message = () => {
 
     return (
         <div className="chat-message overflow-auto px-5">
-            <h1>messages</h1>
+            <div className="text-break mb-2">
+              <b>Имя</b>
+              ": "
+              "сообщение"
+            </div>
         </div>
     )
 }
 
-const MessageHeader = () => {
+/*const MessageHeader = () => {
+
+  return (
+    <div className="bg-light mb-4 p-3 shadow-sm small">
+      <p className="m-0">{currentChannelName}</p>
+    </div>
+    <Message/>
+  )
+}*/
+
+const Messages = () => {
   const channels = useSelector(selectorChannels.selectAll);
   const {currentChannelId} = useSelector((state) => state.channels);
 
   const currentChannelName = channels.filter(({id}) => id === currentChannelId).map((channel) => channel.name);
   return (
-    <div className="bg-light mb-4 p-3 shadow-sm small">
-      <p className="m-0">{currentChannelName}</p>
-    </div>
-  )
-}
-
-const Messages = () => {
-  return (
   <Col className="h-100 p-0">
       <div className="d-flex flex-column h-100">
-        <MessageHeader />
+        <div className="bg-light mb-4 p-3 shadow-sm small">
+          <p className="m-0">{currentChannelName}</p>
+        </div>
+        <Message />
         <h1>messages</h1>
       </div>
   </Col>
