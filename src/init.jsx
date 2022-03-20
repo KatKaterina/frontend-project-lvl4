a98b15c8df69d0b7b20ac005bc10f4be282ee51b
Salnikova Katerina <katya-salnikova@mail.ru>
2022-03-20 23:08:37 +0300

    ref components and slices

--- src/init.jsx
+++ src/init.jsx
@@ -5,16 +5,16 @@ import store from './slices/index.js';
 import { io } from "socket.io-client";
 import { addMessage } from './slices/messagesSlice.js';
 
-//export default async (socketClient = io()) => {
-export default async () => {
-  /*socketClient.on('newMessge', (message) => {
+export default async (socketClient = io()) => {
+//export default async () => {
+  socketClient.on('newMessge', (message) => {
     store.dispatch(addMessage({ message }));
-  });*/
+  });
 
   return (
     <div id="test">
       <Provider store={store}>
-        <App />
+        <App socket={socketClient}/>
       </Provider>
     </div>
   );
