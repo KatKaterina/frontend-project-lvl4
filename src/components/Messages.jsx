779a891ed8cb2a913519015d6895ae67a80b3878
Salnikova Katerina <katya-salnikova@mail.ru>
2022-03-20 18:57:32 +0300

    ref conponents

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -18,7 +18,8 @@ const ChannelMessages = () => {
 };
 
 const FormMessage = () => {
-  const handleSubmit = () => {
+  
+  const handlerSubmit = () => {
 
   }
 
@@ -27,30 +28,35 @@ const FormMessage = () => {
       message: '',
     },
     /*validationSchema: schema,*/
-    onSubmit: handleSubmit,
+    onSubmit: handlerSubmit,
   });
 
   return (
     <div className="mt-auto px-5 py-3">
       <Form onSubmit={formik.handleSubmit} className="py-1 border rounded-2">
-        <InputGroup>
-          <Form.Control 
-            name="body" 
-            aria-label="body"
-            className="border-0 p-0 ps-2"
-            onChange={formik.handleChange}
-            value={formik.values.message}
-          />
-          <InputGroup.Append>
-            <Button type="submit">
-              Send
-            </Button>
-          </InputGroup.Append>
-        </InputGroup>
+      
       </Form>
     </div>
   )
 };
+
+/*<InputGroup>
+<Form.Control 
+  name="body" 
+  aria-label="body"
+  className="border-0 p-0 ps-2"
+  onChange={formik.handleChange}
+  value={formik.values.message}
+/>
+<InputGroup.Append>
+  <Button type="submit">
+    Send
+  </Button>
+</InputGroup.Append>
+</InputGroup>
+*/
+
+
 
 /*const MessageHeader = () => {
 
@@ -74,7 +80,7 @@ const Messages = () => {
           <p className="m-0">{currentChannelName}</p>
         </div>
         <ChannelMessages />
-        
+        <FormMessage />
       </div>
   </Col>
   );
