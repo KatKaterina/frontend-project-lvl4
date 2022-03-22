3d77bd554fc331413ce628384ebc9dbd648820b8
Salnikova Katerina <katya-salnikova@mail.ru>
2022-03-22 16:12:35 +0300

    ref slices

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -51,7 +51,7 @@ const FormMessage = ({ currentChannelId, setUploaded }) => {
         setSubmitting(false);
         resetForm();
         refInput.current.focus();
-        //dispatch(fetchMessages());
+        dispatch(fetchMessages());
         //dispatch(addMessage({ newMessage }));
         //setUploaded(true);
         //dispatch(fetchData());
