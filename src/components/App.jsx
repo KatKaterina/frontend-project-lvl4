0a0f525a327673434af946591b86c49fc0b36a5a
Salnikova Katerina <katya-salnikova@mail.ru>
2022-03-23 17:36:51 +0300

    ref components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -52,14 +52,15 @@ const UserRoute = ({ children }) => {
 };
 
 const renderModal = ({ type }) => {
-  switch (type) {
+ /* switch (type) {
     case 'addChannel': {
       return <ModalAddChannel />
     }
     default: {
       return null;
     }
-  }
+  }*/
+  return null;
 }
 
 const App = ({ socket }) => {
