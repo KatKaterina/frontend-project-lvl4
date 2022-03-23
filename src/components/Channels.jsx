7120e9d398abcc97107d03309c33c0f782bcdcc5
Salnikova Katerina <katya-salnikova@mail.ru>
2022-03-23 12:01:39 +0300

    ref components

--- src/components/Channels.jsx
+++ src/components/Channels.jsx
@@ -4,7 +4,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import { selectorChannels, changeCurrentChannel } from '../slices/ChannelsSlice.js';
 //as={Button} variant={buttonVariant} block
 const fixedChannel = ({ name, buttonVariant, onClick }) => (
-  <Nav.Link variant={buttonVariant}  className="w-100 rounded text-start btn btn-secondary" onClick={onClick}>
+  <Nav.Link as="button" variant={buttonVariant}  className="w-100 rounded text-start btn btn-secondary" onClick={onClick}>
       {name}
     </Nav.Link>
   );
