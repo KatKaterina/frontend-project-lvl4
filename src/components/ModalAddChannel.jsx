7c14afb55bf0d0b524a8e2eb43def82b6dbcd556
Salnikova Katerina <katya-salnikova@mail.ru>
2022-03-28 02:38:15 +0300

    fix toastify

--- src/components/ModalAddChannel.jsx
+++ src/components/ModalAddChannel.jsx
@@ -37,7 +37,12 @@ const FormAddChannel = ({ handleClose, t }) => {
             //dispatch(closeModal());
             //dispatch(changeCurrentChannel({ id }));
             handleClose();
-            toast.success(t('toast.addedChannel'));
+            toast.success(t('toast.addedChannel'),  {
+              position: "top-right",
+              autoClose: 5000,
+              hideProgressBar: false,
+              closeOnClick: true,
+              pauseOnHover: true,} );
            
           } else {
             toast.error(t('toast.connectError'));
