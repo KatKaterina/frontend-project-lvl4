71b70f85d9c9eb8df3b7ff26cec788f8cfde8754
Salnikova Katerina <katya-salnikova@mail.ru>
2022-03-28 14:27:46 +0300

    fix components

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -43,7 +43,7 @@ const FormMessage = ({ currentChannelId, t }) => {
 
   const handlerSubmit = ({ message }, { resetForm, setSubmitting }) => {
     setSubmitting(true);
-    const filteredMessage = filter.check(message) ? filter.clean(message, '+') : message.trim();
+    const filteredMessage = filter.check(message) ? filter.clean(message, '\\*') : message.trim();
     const newMessage =  { message: filteredMessage, channelId: currentChannelId, username };
 
     socket.emit('newMessage', newMessage, (response) => {
