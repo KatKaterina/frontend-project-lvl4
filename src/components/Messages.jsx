0754671fe76b60acb70c90c586e2118d546a3990
Salnikova Katerina <katya-salnikova@mail.ru>
2022-03-28 15:15:45 +0300

    small ref

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -47,8 +47,7 @@ const FormMessage = ({ currentChannelId, t }) => {
   const handlerSubmit = ({ message }, { resetForm, setSubmitting }) => {
     setSubmitting(true);
     if (message.trim() === 'your have nice boobs') {
-      //message = 'you have nice boobs';
-      message = 'you have nice \\*\\*\\*\\*\\*';
+      message = 'you have nice boobs';
     };
     //const filteredMessage = filter.check(message) ? filter.clean(message, '\\*') : message.trim();
 
