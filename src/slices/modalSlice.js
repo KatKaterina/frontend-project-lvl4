90327aa58f0bf10bb31455e59454e0a4323c0dd1
Katerina Salnikova <katya-salnikova@mail.ru>
2022-03-29 19:20:13 +0300

    ref components

--- src/slices/modalSlice.js
+++ src/slices/modalSlice.js
@@ -13,11 +13,15 @@ const modalSlice = createSlice ({
         openModal: (state, { payload }) => {
             //console.log(payload);
             const { type, updateData } = payload;
+            console.log('открыли окно ' + type);
+            console.log(updateData.name);
             state.type = type;
             state.isOpen = true;
             state.updateData = updateData;
         },
         closeModal: (state) => {
+            console.log('закрыли ' + state.type);
+            console.log(state.updateData.name);
             state.isOpen = false;
             state.type = '';
             state.updateData = null;
